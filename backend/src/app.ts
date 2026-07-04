109a359484485b9ae328d15e3da14a516cd3c602
Sumeet203 <sumeetkothiyal203@gmail.com>
2026-07-04 16:18:55 +0530

    Initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import runGraph from "./ai/graph.ai.js"
+const app = express();
+
+app.get("/",async (req,res)=>{
+    const result = await runGraph("Write an code for Factorial Function in js")
+    res.json(result);
+});
+
+export default app;
